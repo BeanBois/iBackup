b01de0cd54ae2cdddc2a7735f77ce400741e2cf4
Kai MBP <kaishun.teoh@gmail.com>
2025-02-14 07:54:29 +0000

    sh file update

--- submit_model.sh
+++ submit_model.sh
@@ -3,11 +3,11 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=<your_username>
 
-WORK_DIR="/vol/bitbucket/kst24/iso"
+WORK_DIR="/vol/bitbucket/kst24/iso/iBackup"
 CUDA_VERSION="11.8.0"
 
-export PATH=/vol/bitbucket/kst24/mml-cw/:$PATH
+export PATH=/vol/bitbucket/kst24/iso/iBackup/:$PATH
 source "/vol/cuda/${CUDA_VERSION}/setup.sh"
 
 pip3 install -r "${WORK_DIR}/requirements.txt"
-python3 -u "${WORK_DIR}/main.py"
+python3 -u "${WORK_DIR}/train.py"
